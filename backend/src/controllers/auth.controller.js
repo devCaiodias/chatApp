20d3741d27f5921cf9c586094412ad6115d0d949
devCaiodias <caiodiasmartins12@gmail.com>
2025-03-29 12:06:48 -0300

    cabei de fazer o metado de login da aplicação

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -49,8 +49,31 @@ export const Signup = async (req, res) => {
     }
   }
 
-export const Login = (req, res) => {
-    res.send("login route");
+export const Login = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+      const user = await User.findOne({email})
+      if (!user) {
+        return res.status(400).json({menssage: "Credenciais invalidas"});
+      }
+
+      const isPasswordCorrect = await bcrypt.compare(password, user.password)
+      if(!isPasswordCorrect) {
+        return res.status(400).json({menssage: "Credenciais invalidas"});
+      }
+
+      genereteToken(user._id, res)
+
+      res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        profilePic: user.profilePic,
+      })
+    } catch (error) {
+      console.log("Error no controller de login", error.menssage)
+      res.status(500).json({menssage: "Internal server error"});
+    }
   }
 
 export const Logout = (req, res) => {
